81f28dde909c772b3bef3a1c2b84514591c0eec1
Денис <eltimccc@yandex.ru>
2022-09-14 16:27:52 +0300

    сделал для title permissions views serializer и url, но перестало работать

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from api.permissons import IsAdminOrReadOnly
+from api.serializers import TitleSerializer
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from reviews.models import Title
 
-# Create your views here.
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = PageNumberPagination
+    
+    def perform_create(self, serializer):
+        serializer.save()
