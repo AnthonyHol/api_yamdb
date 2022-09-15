e170c794861df3da2b20f45854c13b2a9b9f4cb9
Leonid <you-2-me@yandex.ru>
2022-09-16 00:24:54 +0300

    Update admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 
-from .models import (Genre, Category, Comment, Review, Title, User)
+from .models import Genre, Category, Title
 
-@admin.register(Genre)
+#@admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'slug')
     search_fields = ('name',)
 
-@admin.register(Category)
+#@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'slug')
     search_fields = ('name',)
 
+"""
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date')
@@ -21,19 +22,16 @@ class CommentAdmin(admin.ModelAdmin):
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'score', 'pub_date')
     search_fields = ('text',)
+"""
 
-@admin.register(Title)
+#@admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'year', 'category', 'description')
     search_fields = ('name',)
 
+"""
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "role")
     empty_value_display = "-пусто-"    
-"""
-@admin.register(TitleGenre)
-class TitleGenreAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title_id', 'genre_id')
-    search_fields = ('title_id', 'genre_id',)
 """
