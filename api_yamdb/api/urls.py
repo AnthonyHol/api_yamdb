aae70c6b35a9b74ca968930c83fecbe3afe19c5b
Денис <eltimccc@yandex.ru>
2022-09-15 10:39:39 +0300

    сделал views urls для genres и category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,11 @@
-from api.views import TitleViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('titles', TitleViewSet)
+router.register('categories', CategoryViewSet)
+router.register('genres', GenreViewSet)
 
 urlpatterns = [
     path("v1/", include(router.urls), name="v1"),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,22 @@
 from api.permissons import IsAdminOrReadOnly
-from api.serializers import TitleSerializer
-from rest_framework import viewsets
+from api.serializers import CategorySerializer, TitleSerializer
+from rest_framework import permissions, viewsets
 from rest_framework.pagination import PageNumberPagination
-from reviews.models import Title
+from reviews.models import Category, Genre, Title
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
