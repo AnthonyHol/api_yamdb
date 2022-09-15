5454bbcdcec444b0547e5a6f84fa974aedceaf58
Leonid <you-2-me@yandex.ru>
2022-09-16 01:19:02 +0300

    Update admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,36 +1,8 @@
 from django.contrib import admin
 
-from .models import Genre, Category, Title
+# Register your models here.
+from .models import Category, Genre, Title
 
-#@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'slug')
-    search_fields = ('name',)
-
-#@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'slug')
-    search_fields = ('name',)
-
-"""
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date')
-    search_fields = ('text',)
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'score', 'pub_date')
-    search_fields = ('text',)
-
-
-@admin.register(Title)
-class TitleAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'year', 'category', 'description')
-    search_fields = ('name',)
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "role")
-    empty_value_display = "-пусто-"    
-"""
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Title)
