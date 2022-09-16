a138b51bc69863b79ff54f17498ac2c1411dc1da
AnthonyHol <1tonyhol1@gmail.com>
2022-09-16 13:06:45 +0300

    Refactor router in api_url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import APIGetToken, APISignUp, UsersViewSet
 
-router = SimpleRouter()
+router = DefaultRouter()
 
 router.register("users", UsersViewSet, basename="users")
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
+    path("v1/", include(router.urls), name="v1"),
     path("v1/auth/token/", APIGetToken.as_view(), name="get_token"),
     path("v1/auth/signup/", APISignUp.as_view(), name="sign_up_token"),
 ]
